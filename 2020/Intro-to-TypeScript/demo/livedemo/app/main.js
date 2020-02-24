4273323211dfc6e30e16c3891fef56f8ae2ce2f3
Kelly Hutchins <khutchins@esri.com>
2020-02-24 11:28:40 -0800

    Updates to live demo to do a convert js to ts demo

--- 2020/Intro-to-TypeScript/demo/livedemo/app/main.js
+++ 2020/Intro-to-TypeScript/demo/livedemo/app/main.js
@@ -1,36 +1,53 @@
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-define(["require", "exports", "esri/WebMap", "esri/views/MapView", "esri/widgets/LayerList"], function (require, exports, WebMap_1, MapView_1, LayerList_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    WebMap_1 = __importDefault(WebMap_1);
-    MapView_1 = __importDefault(MapView_1);
-    LayerList_1 = __importDefault(LayerList_1);
-    var map = new WebMap_1.default({
-        portalItem: {
-            id: "d5dda743788a4b0688fe48f43ae7beb9"
-        }
-    });
-    // Add the map to a MapView
-    var view = new MapView_1.default({
-        container: "viewDiv",
-        map: map
-    });
-    // Add a legend instance to the panel of a
-    // ListItem in a LayerList instance
-    var layerList = new LayerList_1.default({
-        view: view,
-        listItemCreatedFunction: function (event) {
-            var item = event.item;
-            if (item.layer.type != "group") {
-                item.panel = {
-                    content: "legend",
-                    open: true
-                };
-            }
-        }
-    });
-    view.ui.add(layerList, "top-right");
+require([
+  "esri/Map",
+  "esri/layers/FeatureLayer",
+  "esri/views/MapView",
+  "esri/geometry/geometryEngine"
+], function (Map, FeatureLayer, MapView, geometryEngine) {
+
+  var map = new Map({
+    basemap: "topo-vector"
+  });
+
+  var view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-105.29, 39.93],
+    zoom: 14
+  });
+
+  var measureThisAction = {
+    title: "Measure",
+    id: "measure-this",
+    className: "esri-icon-measure"
+  };
+
+  var popupTemplate = {
+    title: "Trail run",
+    content: "{Name}",
+    overwriteActions: true,
+    actions: [measureThisAction]
+  };
+
+  var featureLayer = new FeatureLayer({
+    url: "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/OSMPTrails/FeatureServer/0",
+    outFields: ["*"],
+    popupTemplate: popupTemplate
+  });
+  map.add(featureLayer);
+
+
+
+
+  view.popup.on("trigger-action", function (event) {
+    if (event.action.id === "measure-this") {
+      var geom = view.popup.selectedFeature.geometry;
+      var distance = geometryEngine.geodesicLength(geom, "miles");
+      distance = parseFloat((Math.round(distance * 100) / 100).toFixed(2));
+      view.popup.content =
+        view.popup.selectedFeature.attributes.Name + " trail is " +
+        distance +
+        " miles long.";
+    }
+  });
 });
-//# sourceMappingURL=main.js.map
